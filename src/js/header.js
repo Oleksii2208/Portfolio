bbaa7c161793063baeefc1099bec37c5f5d1d22d
Oleksiy Kost <AlekseyKo2208@gmail.com>
2026-09-25 06:18:00 +0300

    styles hero and header

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,8 @@
+const refs = {
+  navList: document.querySelector('.menu-link-header'),
+  navMenu: document.querySelector('.nav-header'),
+};
 
+navList.addEventListener('click', () => {
+  navMenu.classList.remove('visually-hidden');
+});
